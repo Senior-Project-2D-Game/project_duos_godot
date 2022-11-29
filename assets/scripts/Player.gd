80bd2d4e79976c05d32675b04759f9fbedd40d75
Sean Ebenmelu <jindusean@gmail.com>
2022-11-28 23:19:12 -0500

    Update Player.gd

--- assets/scripts/Player.gd
+++ assets/scripts/Player.gd
@@ -7,6 +7,7 @@ const JUMP_FORCE = 300
 const UP_DIRECTION = Vector2.UP
 
 var is_master = false
+var is_player1 = false
 var player_state = "normal"
 var _velocity = Vector2.ZERO
 export var controls : Resource = null
@@ -40,7 +41,34 @@ func _physics_process(delta):
 				_velocity.y = -JUMP_FORCE + abs(_velocity.y)/2
 			elif is_jump_cancelled:
 				_velocity.y = 0.0
+	else:
+		var is_falling = _velocity.y > 0.0 and not is_on_floor()
+		var is_jumping = Input.is_action_just_pressed(controls.jump) and is_on_floor()
+		var is_jump_cancelled = Input.is_action_just_released(controls.jump) and _velocity.y < 0.0
+		var is_idling = is_on_floor() and is_zero_approx(_velocity.x)
+		var is_running = is_on_floor() and not is_zero_approx(_velocity.x)
+		var is_launched = player_state == "launched"
+		var is_sliding = player_state == "sliding"
+		
+		var _horizontal_direction = (
+			Input.get_action_strength(controls.move_right)
+			- Input.get_action_strength(controls.move_left)
+		)
+		_velocity.y += GRAVITY * delta 
+		
+		if is_sliding:
+			_velocity.x = sign(_velocity.x) * SPEED
 			
+			if is_on_wall():
+				_velocity.x = -_velocity.x
+		else:
+			_velocity.x = _horizontal_direction * SPEED
+			if is_jumping:
+				_velocity.y = -JUMP_FORCE
+			elif is_launched:
+				_velocity.y = -JUMP_FORCE + abs(_velocity.y)/2
+			elif is_jump_cancelled:
+				_velocity.y = 0.0	
 
 	_velocity = move_and_slide(_velocity, UP_DIRECTION)
 	rpc_unreliable("update_position", position)
