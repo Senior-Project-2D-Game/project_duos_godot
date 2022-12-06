344d7071fa8ca3224f686603d3b046cf7a255a97
Sean Ebenmelu <jindusean@gmail.com>
2022-12-06 02:57:16 -0500

    lava animation and death colliders
    added lava animation and death colliders. So far the colliders detect foreign bodies but don't kill the player.

--- assets/scripts/Area2D.gd
+++ assets/scripts/Area2D.gd
@@ -1,21 +1,10 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Area2D_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if body is KinematicBody2D:
 		self.shape_owner_get_owner(local_shape_index).queue_free()
+
+# handles body collision with death collider
+func _on_Death_Collides_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if body is KinematicBody2D:
+		print("Dead")
