a0ad6222ed122109821e8018ba1f73bcd1251926
Henry Baldacci <henry.louis.980@gmail.com>
2023-01-24 13:37:32 -0500

    Fixed bug with death triggers

--- scenes/Levels/Fire Ice Level.gd
+++ scenes/Levels/Fire Ice Level.gd
@@ -8,12 +8,10 @@ onready var players := {
 	"1": {
 		viewport = $"VBoxContainer/ViewportContainer/Viewport",
 		camera = $"VBoxContainer/ViewportContainer/Viewport/Camera2D",
-		player = $"VBoxContainer/ViewportContainer/Viewport/Level/FirePlayer",
 	},
 	"2": {
 		viewport = $"VBoxContainer/ViewportContainer2/Viewport",
 		camera = $"VBoxContainer/ViewportContainer2/Viewport/Camera2D",
-		player = $"VBoxContainer/ViewportContainer/Viewport/Level/IcePlayer",
 	}
 }
 
@@ -21,12 +19,14 @@ func _ready():
 	var player_arr = Players.get_children()
 	player_arr[0].get_parent().remove_child(player_arr[0])
 	player_arr[1].get_parent().remove_child(player_arr[1])
+	player_arr[0].set_name("FirePlayer")
+	player_arr[1].set_name("IcePlayer")
 	player_arr[0].position = Vector2(-600,-100)
 	player_arr[1].position = Vector2(-600,900)
 	var fire_sprite = Sprite.new()				# comment for animation
-	fire_sprite.set_name("FirePlayer")			# comment for animation
+	#fire_sprite.set_name("FirePlayer")			# comment for animation
 	var ice_sprite = Sprite.new()				# comment for animation
-	ice_sprite.set_name("IcePlayer")			# comment for animation
+	#ice_sprite.set_name("IcePlayer")			# comment for animation
 #	player_arr[0].add_child(ice_player.instance())		# uncomment for animation
 #	player_arr[1].add_child(fire_player.instance())		# uncomment for animation	
 	player_arr[0].add_child(ice_sprite)
